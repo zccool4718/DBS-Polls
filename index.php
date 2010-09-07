81d210ed27a7d2b615d86bcef05de80d3c5a31f1
Timothy Dunn <tdunn@td14.com>
2010-09-06 22:07:10 -0500

    FC with facebook template

--- index.php
+++ index.php
@@ -7,6 +7,33 @@
 
 include("header.php");
 
+$uid = $facebook->getUser();
+		
+		# let's check if the user has granted access to posting in the wall
+		$api_call = array(
+			'method' => 'users.hasAppPermission',
+			'uid' => $uid,
+			'ext_perm' => 'publish_stream'
+		);
+		$can_post = $facebook->api($api_call);
+		if($can_post){
+			# post it!
+			# $facebook->api('/'.$uid.'/feed', 'post', array('message' => 'Saying hello from my Facebook app!'));
+			
+			# using all the arguments
+			$facebook->api('/'.$uid.'/feed', 'post', array(
+				'message' => 'The message',
+				'name' => 'The name',
+				'description' => 'The description',
+				'caption' => 'The caption',
+				'picture' => 'http://i.imgur.com/yx3q2.png',
+				'link' => 'http://net.tutsplus.com/'
+			));
+			echo 'Posted!';
+		} else {
+			die('Permissions required!');
+		}
+
 ?>
     <script type="text/javascript">
         $(document).ready(function(){
