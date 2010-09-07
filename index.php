64024be02e7de436527286310a1fe8e148755bcf
Timothy Dunn <tdunn@td14.com>
2010-09-06 20:52:15 -0500

    FC with facebook template

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
  * @copyright 2010
  */
 
+require("includes/facebook.php");
+require("includes/config.php");
+require("includes/common.php");
+require("classes/class.database.php");
 include("header.php");
+
+$database = new database(getDBinfo());
 
             $sql = "SELECT * FROM users WHERE oauth_provider = 'facebook' AND oauth_uid = ". $user['id'];
             $result = $database->query($sql);
