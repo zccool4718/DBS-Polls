1e275c54e99cf9230fa22b9109e9a1cec54ca739
Timothy Dunn <tdunn@td14.com>
2010-09-07 19:16:33 -0500

    styling

--- newPoll.php
+++ newPoll.php
@@ -52,7 +52,7 @@ $accessToken = $facebook->getAccessToken();
     <table width="100%" border="0" class="poll">
         <thead>
             <tr>
-                <th rowspan="2"> Create a Poll </th>
+                <th calspan="2"> Create a Poll </th>
             </tr>
         </thead>
         <tbody>
