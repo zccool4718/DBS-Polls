e47e1005aec6f152a5a8dfcbd1c73bc27ae0a961
Timothy Dunn <tdunn@td14.com>
2010-09-09 16:56:57 -0500

    styling changes

--- ajax.php
+++ ajax.php
@@ -6,6 +6,15 @@
  */
 
 $_POST['options'] = explode("\n", $_POST['options']);
+$_POST['optionsCount'] = count($_POST['options']);
+
+if(count($_POST['options']) > 6){
+    $tmp = count($_POST['options']) - 6;
+    
+    $tmp = round($tmp / 3);
+    
+    $_POST['price'] += $tmp;
+}
 
 print_r($_POST);
 ?>
