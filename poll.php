0657a23d682baa366fdd7a877ae2efca76b495b5
Timothy Dunn <tdunn@td14.com>
2010-09-12 02:40:33 -0500

    Style update

--- poll.php
+++ poll.php
@@ -49,11 +49,12 @@ if(!isset($_GET['ID'])){
     foreach($result as $index => $value){
         $results[count($results) - 1] = 0;
     }
-    
+    print_r($results);
     foreach($pollResults as $index => $value){       
         $results[$value['answers']] = $results[$value['answers']] + 1;
     }
-        
+     
+    print_r($results);   
 
 
 } else {
